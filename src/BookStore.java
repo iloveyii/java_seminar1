3bf1071d0a0d0f912a10ce7ee217bb10d78bf1bb
Hazrat Ali <iloveyii@yahoo.com>
2020-12-27 17:57:46 +0100

    Added exception handling

--- src/BookStore.java
+++ src/BookStore.java
@@ -17,7 +17,7 @@ import java.util.*;
 	        if(remainder == 0) {
 		        System.out.printf("You will receive %d boxes containing 5 books each.", boxesCount);
 	        } else {
-		        System.out.printf("You will receive %d boxes, %d boxes containing 5 books each and one box containing %d books.", boxesCount, boxesCount - 1, this.booksCount % 5 );
+		        System.out.printf("You will receive %d boxes, %d boxes containing 5 books each and one box containing %d book(s).", boxesCount, boxesCount - 1, this.booksCount % 5 );
 	        }
 	        System.out.println();
 	        System.out.printf("The shipping price will be: %.2f sek", shippingCost);
@@ -44,10 +44,17 @@ import java.util.*;
 		
 		public static void task1() {
 			int greaterThanZero = 1;
+			int booksCount;
 			while(greaterThanZero > 0) {
 				System.out.println();
 				System.out.print("How many books do you have for shipping? ");
-				int booksCount = userInput.nextInt();
+				try {
+					booksCount = userInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid number of books, enter an intger");
+					userInput.nextLine();
+					continue;
+				}
 	            if (booksCount < 1) {
 	                System.out.println("Not a valid number, try again.");
 	            } else {
@@ -61,25 +68,70 @@ import java.util.*;
 		public static void task2() {
 			int a = 0;
 			int minimum = a;
+			boolean correct = false;
+			
 			System.out.println();
 			System.out.println("Enter a few numbers to find the minumum, 0 to exit. ");
-			System.out.println("Write a number:");
-			a = userInput.nextInt();
+			while(!correct) {
+				try {
+					System.out.println("Write a number:");
+					a = userInput.nextInt();
+					correct = true;
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid number, enter an integer");
+					userInput.nextLine();
+					continue;
+				}
+			}
 			minimum = a;
 			while (a > 0) {
-				System.out.println("Write another number:");
-				a = userInput.nextInt();
+				System.out.println("Write another number (0 -exit):");
+				try {
+					a = userInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid number, enter an integer");
+					userInput.nextLine();
+					continue;
+				}
+				
 				if(a != 0 && a < minimum) minimum = a;	
 			} 
 			System.out.println("The minimum number that you have entered was: " + minimum);
 		}
 		
 		public static void task3() {
+			int averageSpeed = 1;
+			double mileage = 1.0;
+			boolean correct = false;
+			
 			System.out.println();
-			System.out.println("Enter the average speed <km/h>:");
-			int averageSpeed = userInput.nextInt();
-			System.out.println("Enter the remaining mileage <mile>:");
-			double mileage = userInput.nextDouble();
+						
+			while(!correct) {
+				try {
+					System.out.println("Enter the average speed <km/h>:");
+					averageSpeed = userInput.nextInt();
+					correct = true;
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid average speed, enter an integer");
+					userInput.nextLine();
+					continue;
+				} 
+			}
+			
+			correct = false;
+			
+			while(!correct) {
+				try {
+					System.out.println("Enter the remaining mileage <mile>:");
+					mileage = userInput.nextDouble();
+					 correct = true;
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid mileage, enter a double number");
+					userInput.nextLine();
+					continue;
+				} 
+			}
+			
 			mileage = mileage * 10; // in km
 			double remainingTime = mileage / averageSpeed;
 			remainingTime = remainingTime * 60; // in min
@@ -90,7 +142,7 @@ import java.util.*;
 		
 		public static void main(String[] args) {
 			int choice = 0;
-			do {
+			while(true) {
 				System.out.println();
 				System.out.println();
 				System.out.println("1. Taks 1");
@@ -98,7 +150,15 @@ import java.util.*;
 				System.out.println("3. Taks 3");
 				System.out.println("0. Exit");
 				System.out.print("Choose the task number (1-3), 0 to exit : ");
-				choice = userInput.nextInt();
+
+				try {
+					choice = userInput.nextInt();
+				} catch (InputMismatchException e) {
+					userInput.nextLine();
+					System.out.println("Invalid choice, enter an integer (0-3).");
+					continue; 
+				}
+				
 				switch(choice) {
 				case 1:
 					task1();
@@ -109,8 +169,10 @@ import java.util.*;
 				case 3:
 					task3();
 					break;
+				case 0:
+					return;
 				}			
-			} while (choice != 0);
+			} 
 		}
 	}
 	
